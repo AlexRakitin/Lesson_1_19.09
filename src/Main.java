6e203e1f20473098502695051518a5f5c86a3f24
Aliaksandr Rakitsin <aliaksandr.rakitsin@upb.lv>
2022-09-20 23:15:48 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+        public static void main(String[] args) {
+            String A = "Hello";
+            String B = "World";
+            System.out.println(A+" " + B);
+        }
+    }
+
